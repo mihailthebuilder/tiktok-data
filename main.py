09db5a52de259948fcd01de12577f21ba64f8222
mmarian <mihailmarian12@gmail.com>
2023-12-22 19:28:05 +0000

    logging

--- main.py
+++ main.py
@@ -1,14 +1,23 @@
 import asyncio
 from playwright.async_api import async_playwright
+import logging
 
 
 async def main():
+    logging.basicConfig(
+        format="%(asctime)s %(levelname)-8s %(message)s", level=logging.INFO
+    )
+
+    logging.info("START")
+
     async with async_playwright() as p:
         browser = await p.chromium.launch()
         page = await browser.new_page()
         await page.goto("http://playwright.dev")
         print(await page.title())
         await browser.close()
+
+    logging.info("END")
 
 
 asyncio.run(main())
