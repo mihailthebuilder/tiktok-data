04778f3dccc024c0e54c70490e1f3039a642b9db
mmarian <mihailmarian12@gmail.com>
2024-01-02 17:29:27 +0000

    attempt to upload data failed

--- main.py
+++ main.py
@@ -4,6 +4,8 @@ import os
 from pathlib import Path
 import httpx
 from pydantic import BaseModel
+import psycopg
+from dotenv import load_dotenv
 
 
 class TrendValueAtTimestamp(BaseModel):
@@ -24,6 +26,7 @@ def main():
     logging.basicConfig(
         format="%(asctime)s %(levelname)-8s %(message)s", level=logging.INFO
     )
+    load_dotenv()
 
     log("START")
 
@@ -71,9 +74,72 @@ def main():
 
     hashtags = hashtags + parse_popular_hashtags_json(res.json())
 
-    produce_report(hashtags)
+    log("exporting hashtags")
+    export_hashtags(hashtags)
+
+    log("uploading data")
+    upload_data(hashtags)
 
     log("END")
+
+
+def upload_data(hashtags: list[Hashtag]):
+    db_url = os.environ["DATABASE_URL"]
+
+    with psycopg.connect(db_url) as conn:
+        cur = conn.cursor()
+
+        cur.execute(
+            """
+                UPDATE hashtag
+                SET "latest_trending" = FALSE
+                WHERE 
+                    "country_code" = %s
+                    AND "latest_trending" = TRUE
+            """,
+            ["US"],
+        )
+
+        for hashtag in hashtags:
+            inserted_hashtag = cur.execute(
+                """
+                    INSERT INTO hashtag 
+                        ("name", "country_code", "posts", "rank", "latest_trending", "views")
+                    VALUES
+                        (%s, %s, %s, %s, %s, %s)
+                    RETURNING 
+                        id
+                """,
+                [
+                    hashtag.hashtag_name,
+                    "US",
+                    hashtag.publish_cnt,
+                    hashtag.rank,
+                    True,
+                    hashtag.video_views,
+                ],
+            )
+            inserted_hashtag_result = inserted_hashtag.fetchone()
+            if inserted_hashtag_result == None:
+                raise Exception(f"couldn't insert hashtag {hashtag.hashtag_name}")
+
+            inserted_hashtag_id = inserted_hashtag_result[0]
+
+            if type(inserted_hashtag_id) is not str:
+                raise Exception(
+                    f"couldn't get uuid of inserted hashtag {hashtag.hashtag_name}"
+                )
+
+            for trend in hashtag.trend:
+                cur.execute(
+                    """
+                        INSERT INTO hashtag_interest
+                            ("hashtag_id", "recorded_for", "interest")
+                        VALUES
+                            (%s, %s, %s)
+                    """,
+                    [inserted_hashtag_id, trend.time, round(trend.value * 100)],
+                )
 
 
 def parse_popular_hashtags_json(input: dict) -> list[Hashtag]:
@@ -90,7 +156,7 @@ def set_tiktok_api_headers(req: Request, headers: list[tuple[str, str]]):
                 headers.append((header["name"], header["value"]))
 
 
-def produce_report(hashtags: list[Hashtag]):
+def export_hashtags(hashtags: list[Hashtag]):
     with open("output.txt", "w", encoding="utf-8") as f:
         for hashtag in hashtags:
             f.write(f"{hashtag.model_dump()}\n")
--- main_test.py
+++ main_test.py
@@ -1,7 +1,7 @@
 import unittest
 import json
 
-from main import parse_popular_hashtags_json, produce_report
+from main import parse_popular_hashtags_json, export_hashtags
 
 
 class TestParsePopularHashtagsJson(unittest.TestCase):
@@ -12,9 +12,9 @@ class TestParsePopularHashtagsJson(unittest.TestCase):
             self.assertEqual(len(hashtags), 50)
 
 
-class TestWriteReport(unittest.TestCase):
+class TestExportHashtags(unittest.TestCase):
     def test_happy_path(self):
         with open("test.json", "r", encoding="utf-8") as f:
             api_response = json.load(f)
             hashtags = parse_popular_hashtags_json(api_response)
-            produce_report(hashtags)
+            export_hashtags(hashtags)
